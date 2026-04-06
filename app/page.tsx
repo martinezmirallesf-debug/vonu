76a037d1c08b69023a1009a9c471b4208c037be7
martinezmirallesf-debug <martinezmirallesf@gmail.com>
2026-04-06 09:45:06 +0200

    Reestructuración Vonu 2.81

--- app/page.tsx
+++ app/page.tsx
@@ -5280,9 +5280,9 @@ const isFirstUserMessage =
   key={m.id}
   data-msg-id={m.id}
   className={[
-    "flex w-full justify-end animate-[fadeIn_240ms_ease-out]",
-    isFirstUserMessage ? "mt-10 md:mt-0" : "",
-  ].join(" ")}
+  "flex w-full justify-end animate-[fadeIn_240ms_ease-out]",
+  isFirstUserMessage ? "mt-12 md:mt-0" : "",
+].join(" ")}
 >
                 <div
                   className={[
