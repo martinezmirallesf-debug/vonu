a76e34dd5066485225e5fbbdfa34f657916af8ad
martinezmirallesf-debug <martinezmirallesf@gmail.com>
2026-05-21 19:40:11 +0200

    Adjust mobile home header spacing

--- app/components/HomeHeader.tsx
+++ app/components/HomeHeader.tsx
@@ -54,7 +54,7 @@ export default function HomeHeader() {
 
   return (
     <header className="sticky top-0 z-50 bg-white">
-      <div className="mx-auto max-w-[1500px] px-4 sm:px-6 lg:px-8">
+  <div className="mx-auto max-w-[1500px] px-6 sm:px-6 lg:px-8">
         {/* Desktop */}
         <div className="hidden h-16 items-center justify-between md:flex">
           <Link href="/" aria-label="VonuAI inicio">
