7a75d7249cb724e817f3efcaf546abf1ee545c0b
martinezmirallesf-debug <martinezmirallesf@gmail.com>
2026-09-13 18:14:54 +0200

    style(vonu-check): load final scanner polish

--- app/[locale]/check/page.tsx
+++ app/[locale]/check/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import CheckClient from "./CheckClient";
 import "./check.css";
+import "./check-polish.css";
 import { isSupportedLocale, supportedLocales } from "@/lib/vonu-check/i18n";
 import type { SupportedLocale } from "@/lib/vonu-check/types";
 
