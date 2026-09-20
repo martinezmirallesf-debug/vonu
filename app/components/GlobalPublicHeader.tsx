5a0d191514d77b6139c4f7a8ca44436f5966f009
martinezmirallesf-debug <martinezmirallesf@gmail.com>
2026-09-20 13:19:10 +0200

    feat(nav): expand document resources menu

--- app/components/GlobalPublicHeader.tsx
+++ app/components/GlobalPublicHeader.tsx
@@ -28,7 +28,12 @@ const caseSlugs: IndexedPublicSlug[] = [
 ];
 
 const resourceSlugs: IndexedPublicSlug[] = [
+  "revisar-contrato",
   "revisar-contrato-alquiler",
+  "comprobar-factura",
+  "revisar-presupuesto",
+  "revisar-contrato-servicios",
+  "revisar-prestamo-financiacion",
 ];
 
 const cookies: Record<SupportedLocale, string> = {
